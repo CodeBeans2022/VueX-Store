1e7b17a894015de6f137bb9566abad1b76a77d09
CodeBeans2022 <jamieleekinnear21@gmail.com>
2023-06-28 13:52:15 +0200

    creating store

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    counter: null
   },
   mutations: {
+    addition(state) {
+      state.counter++
+    },
+    subtraction(state) {
+      state.counter--
+    }
   },
   actions: {
+    increment(context) {
+      context.commit('addition')
+    },
+    decrement(context) {
+      context.commit('subtraction')
+    }
   },
   modules: {
   }
---
+++ src/views/CounterView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                <button class="btn btn-success" @click="increment">+</button>
+                
+                </div>
+                <div class="col">
+                    <p class="lead">{{ counter }}</p>
+                </div>
+                <div class="col">
+                <button class="btn btn-danger" @click="decrement">-</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            counter() {
+                return this.$store.state.counter
+            }
+        },
+        methods: {
+            increment() {
+                this.$store.dispatch('increment')
+            },
+            decrement() {
+                this.$store.dispatch('decrement')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
